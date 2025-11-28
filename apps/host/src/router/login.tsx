2ea8816911a7b736012a3889d166ce43572f2887
Viacheslav Konyshev <VAKonyshev@alfabank.ru>
2025-11-28 10:20:12 +0500

    fix(@mf-types): downgrade @module-federation/rsbuild-plugin to 0.19.1

--- apps/host/src/router/login.tsx
+++ apps/host/src/router/login.tsx
@@ -3,8 +3,6 @@ import { createRoute } from '@tanstack/react-router';
 
 import { rootRoute } from './root';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 const Login = React.lazy(() => import('remote/login'));
 
 export const loginRoute = createRoute({
