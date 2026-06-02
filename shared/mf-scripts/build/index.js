d03493d7a3987477683e3646ed1b173f6a21e1af
Viacheslav Konyshev <VAKonyshev@alfabank.ru>
2026-06-02 23:24:59 +0500

    chore(*): add unified ts config, some refactor

--- shared/mf-scripts/build/index.js
+++ shared/mf-scripts/build/index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import { createRsbuild, defineConfig, loadConfig, mergeRsbuildConfig, logger, } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { htmlTemplate } from './html-template.js';
 var Scripts;
 (function (Scripts) {
     Scripts["dev"] = "dev";
@@ -22,26 +23,7 @@ if (isAvailableScript(scriptName)) {
         plugins: [pluginReact()],
         tools: {
             htmlPlugin: {
-                templateContent: `
-        <!doctype html>
-        <html>
-
-        <head>
-          <meta charset="utf-8" />
-          <title>Conduit</title>
-          <!-- Import Ionicon icons & Google Fonts our Bootstrap theme relies on -->
-          <link href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />
-          <link
-            href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
-            rel="stylesheet" type="text/css" />
-        </head>
-
-        <body>
-          <div id="root"></div>
-        </body>
-
-        </html>
-      `,
+                templateContent: htmlTemplate,
                 templateParameters: {
                     mountId: 'root',
                 },
--- shared/mf-scripts/src/index.ts
+++ shared/mf-scripts/src/index.ts
@@ -9,6 +9,8 @@ import {
 } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
+import { htmlTemplate } from './html-template.js';
+
 enum Scripts {
   dev = 'dev',
   build = 'build',
@@ -33,26 +35,7 @@ if (isAvailableScript(scriptName)) {
     plugins: [pluginReact()],
     tools: {
       htmlPlugin: {
-        templateContent: `
-        <!doctype html>
-        <html>
-
-        <head>
-          <meta charset="utf-8" />
-          <title>Conduit</title>
-          <!-- Import Ionicon icons & Google Fonts our Bootstrap theme relies on -->
-          <link href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />
-          <link
-            href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
-            rel="stylesheet" type="text/css" />
-        </head>
-
-        <body>
-          <div id="root"></div>
-        </body>
-
-        </html>
-      `,
+        templateContent: htmlTemplate,
         templateParameters: {
           mountId: 'root',
         },
